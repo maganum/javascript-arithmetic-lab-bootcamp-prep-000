bc86330b27534d320df72d26546906b646cf80fd
Magnum Polmatier <maganum@hotmail.com>
2018-05-04 03:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+var a
+var b
+var n
 
 function add(a,b) {
   return(a+=b)
 }
 function subtract(a,B) {
   return(a-=b)
+}
+function multiply(a,B) {
+  return(a*=b)
+}
+function divide(a,B) {
+  return(a/=b)
+}
+function inc(n) {
+  return(n++)
+}
+function dec(n) {
+  return(n--)
+}
+function parseInt(a,b) {
+  return(parseInt(a,b))
+}
+function parseFloat(a,b) {
+  return(parseFloat(a,b))
 }
